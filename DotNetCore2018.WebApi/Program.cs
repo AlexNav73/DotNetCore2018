26461695f1c2b96e953058d77f7b1a6dd9c2f42c
AlexNav73 <fifthhorseman@mail.ru>
2018-09-20 22:44:33 +0300

    fixed project names

--- DotNetCore2018.WebApi/Program.cs
+++ DotNetCore2018.WebApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace WebApi
+namespace DotNetCore2018.WebApi
 {
     public class Program
     {
--- DotNetCore2018.WebApi/Startup.cs
+++ DotNetCore2018.WebApi/Startup.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace WebApi
+namespace DotNetCore2018.WebApi
 {
     public class Startup
     {
