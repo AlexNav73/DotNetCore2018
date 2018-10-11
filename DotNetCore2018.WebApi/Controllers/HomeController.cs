4cb825d966e389c3f0c6fb4fe613d04dd5f8145d
Aliaksandr_Navitski <Aliaksandr_Navitski@epam.com>
2018-10-11 15:56:36 +0300

    added home error action to vew error page

--- DotNetCore2018.WebApi/Controllers/HomeController.cs
+++ DotNetCore2018.WebApi/Controllers/HomeController.cs
@@ -2,15 +2,34 @@ using System;
 using DotNetCore2018.Business.Services.Interfaces;
 using DotNetCore2018.Business.Specifications;
 using DotNetCore2018.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace DotNetCore2018.WebApi.Controllers
 {
     [Route("[controller]/[action]")]
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Error()
+        {
+            var error = HttpContext.Features.Get<IExceptionHandlerFeature>().Error;
+            _logger.LogError(error, "Caught in global error handling routine: {}", error.Message);
+
             return View();
         }
     }
