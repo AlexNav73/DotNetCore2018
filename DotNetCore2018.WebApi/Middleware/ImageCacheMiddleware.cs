b57ed6df1d6dffb0d9ef7ed05308b75034955849
Aliaksandr_Navitski <Aliaksandr_Navitski@epam.com>
2018-10-16 12:51:37 +0300

    implemented image caching middleware

--- DotNetCore2018.WebApi/Middleware/ImageCacheMiddleware.cs
+++ DotNetCore2018.WebApi/Middleware/ImageCacheMiddleware.cs
@@ -1,11 +1,9 @@
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using DotNetCore2018.Business.Services.Interfaces;
 using DotNetCore2018.Business.Specifications;
 using DotNetCore2018.Data.Entities;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.FileProviders;
 
 namespace DotNetCore2018.WebApi.Middleware
@@ -13,6 +11,7 @@ namespace DotNetCore2018.WebApi.Middleware
     public class ImageCacheMiddleware
     {
         private const string CacheFolder = "cache";
+        private const string CategoryImage = "/category/image";
 
         private readonly RequestDelegate _next;
         private readonly IFileProvider _fileProvider;
@@ -27,15 +26,18 @@ namespace DotNetCore2018.WebApi.Middleware
             {
                 Directory.CreateDirectory(Path.Combine(_wwwroot, CacheFolder));
             }
-            _fileProvider = new PhysicalFileProvider(Path.Combine(_wwwroot, CacheFolder));
+            _fileProvider = new PhysicalFileProvider(_wwwroot);
         }
 
         public async Task InvokeAsync(HttpContext context, IDataService dataService)
         {
-            if (context.Request.Path.StartsWithSegments("/category/image"))
+            if (context.Request.Path.StartsWithSegments(CategoryImage))
             {
-                if (await ReturnCachedFile(context, dataService))
+                var imageName = GetImageId(context);
+                var file = _fileProvider.GetFileInfo($"/{CacheFolder}/{imageName}.jpeg");
+                if (file.Exists)
                 {
+                    await context.Response.SendFileAsync(file);
                     return;
                 }
             }
@@ -44,32 +46,38 @@ namespace DotNetCore2018.WebApi.Middleware
 
             if (context.Response.ContentType == "image/jpeg")
             {
-                await SaveFileToCache(context, dataService);
+                await CacheImage(GetImageId(context), dataService);
             }
         }
 
-        private async Task SaveFileToCache(HttpContext context, IDataService dataService)
+        private async Task CacheImage(string id, IDataService dataService)
         {
-
-        }
-
-        private async Task<bool> ReturnCachedFile(HttpContext context, IDataService dataService)
-        {
-            var id = context.Request.Query["id"].ToArray().FirstOrDefault();
             if (id != null && int.TryParse(id, out int idValue))
             {
                 var category = dataService.GetBy(new IdSpecification<Category>(idValue));
                 if (category?.Image != null)
                 {
-                    var fileInfo = _fileProvider.GetFileInfo($"{category.Image}.jpeg");
+                    var fileInfo = _fileProvider.GetFileInfo($"/images/{category.Image}.jpeg");
                     if (fileInfo.Exists)
                     {
-                        await context.Response.SendFileAsync(fileInfo);
-                        return true;
+                        using (var reader = fileInfo.CreateReadStream())
+                        {
+                            var cacheFilePath = _fileProvider.GetFileInfo($"/{CacheFolder}/{id}.jpeg").PhysicalPath;
+                            using (var writer = File.OpenWrite(cacheFilePath))
+                            {
+                                await reader.CopyToAsync(writer);
+                            }
+                        }
                     }
                 }
             }
-            return false;
+        }
+
+        private string GetImageId(HttpContext context)
+        {
+            return context.Request.Path
+                .ToUriComponent()
+                .Replace(CategoryImage + "/", string.Empty);
         }
     }
 }
