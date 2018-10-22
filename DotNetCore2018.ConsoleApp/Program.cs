9bf9ef9237727c7ab4d7d156e3e5db29995d1803
Aliaksandr_Navitski <Aliaksandr_Navitski@epam.com>
2018-10-22 12:41:03 +0300

    added console app to test WebApi endpoint

---
+++ DotNetCore2018.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+
+namespace DotNetCore2018.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:5000/api/v1/");
+
+                var response = client.GetAsync("categories").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Categories: ");
+                    Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                }
+                response = client.GetAsync("products").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Products: ");
+                    Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                }
+            }
+        }
+    }
+}
