6e4f988073d1e19c45bc3840cfb30ade12432667
Aliaksandr_Navitski <Aliaksandr_Navitski@epam.com>
2018-12-17 13:41:18 +0300

    removed https requirements for production environment

--- DotNetCore2018.WebApi/Startup.cs
+++ DotNetCore2018.WebApi/Startup.cs
@@ -62,14 +62,14 @@ namespace DotNetCore2018.WebApi
             }));
             services.AddSwaggerDocument();
             services.AddSession();
-            if (!_env.IsDevelopment() && !_env.IsEnvironment("Docker"))
+            if (!_env.IsDevelopment() && !_env.IsEnvironment("Docker") && !_env.IsProduction())
             {
                 services.Configure<MvcOptions>(o => o.Filters.Add(new RequireHttpsAttribute()));
             }
 
             services.AddIdentity<User, UserRole>(o =>
             {
-                if (_env.IsDevelopment() || _env.IsEnvironment("Docker"))
+                if (_env.IsDevelopment() || _env.IsEnvironment("Docker") || _env.IsProduction())
                 {
                     o.SignIn.RequireConfirmedEmail = false;
                     o.SignIn.RequireConfirmedPhoneNumber = false;
@@ -158,7 +158,7 @@ namespace DotNetCore2018.WebApi
 
             context.Database.Migrate();
 
-            if (env.IsDevelopment() && env.IsEnvironment("Docker"))
+            if (env.IsDevelopment() && env.IsEnvironment("Docker") && env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseDatabaseErrorPage();
